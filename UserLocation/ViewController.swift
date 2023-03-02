ddfe0de0fb1b233030bdeec8d6a62ffe846fc295
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2023-03-02 10:53:03 +0300

    get user locations

--- UserLocation/ViewController.swift
+++ UserLocation/ViewController.swift
@@ -6,14 +6,43 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    private var locationManager: CLLocationManager?
+
+    private let latLongLabel: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .systemFill
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 20)
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        latLongLabel.frame = CGRect(x: 20, y: view.bounds.height / 2 - 50, width: view.bounds.width - 40, height: 100)
+        view.addSubview(latLongLabel)
+
+        getUserLocation()
     }
 
+    func getUserLocation() {
+        locationManager = CLLocationManager()
+        locationManager?.requestAlwaysAuthorization()
+        locationManager?.startUpdatingLocation()
+
+        locationManager?.delegate = self
+
+        //locationManager?.allowsBackgroundLocationUpdates = true
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            latLongLabel.text = "Lat: \(location.coordinate.latitude) \nLng : \(location.coordinate.longitude)"
+        }
+    }
 
 }
 
