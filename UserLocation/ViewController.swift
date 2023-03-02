e485dc8c003696966a74868b35bac88076cf81f0
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2023-03-02 10:32:36 +0300

    Initial Commit

---
+++ UserLocation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UserLocation
+//
+//  Created by Amby on 02/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
